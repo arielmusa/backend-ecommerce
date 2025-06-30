8081e6986c9cedca1135ac48e212ef6e80505e0e
riccardo-guido <guidoriccardo02@gmail.com>
2025-06-30 19:34:31 +0200

    add orderController, routes and alignment

---
+++ controllers/ordersController.js
@@ -0,0 +1,29 @@
+import { db } from "../config/db.js";
+
+export default function indexOrders(req, res) {
+  const sql =
+    "SELECT * FROM `boolean-ecommerce`.orders ORDER BY `created_at` DESC;";
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(502).json({
+        error: 502,
+        message: "invalid query",
+      });
+    }
+    res.json(results);
+  });
+}
+
+export function showOrders(req, res) {
+  const { id } = req.params;
+  const sql = "SELECT * FROM `boolean-ecommerce`.orders WHERE id = ?;";
+  db.query(sql, [id], (err, results) => {
+    if (err) {
+      return res.status(502).json({ error: 502, message: "invalid query" });
+    }
+    if (results.length === 0) {
+      return res.status(404).json({ error: 404, message: "order not found" });
+    }
+    res.json(results[0]);
+  });
+}
